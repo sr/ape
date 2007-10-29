63a44c89aea846065637d87175282abdcd86ddc6
david_calavera <david_calavera>
2007-10-29 16:41:53 +0000

    invoker bug with ssl

--- src/invoker.rb
+++ src/invoker.rb
@@ -1,6 +1,6 @@
 #   Copyright © 2006 Sun Microsystems, Inc. All rights reserved
 #   Use is subject to license terms - see file "LICENSE"
-
+require 'net/https'
 require 'atomURI'
 require 'crumbs'
 
