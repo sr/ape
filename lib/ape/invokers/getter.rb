157eae6f8cbdc83a3fc66a57c7c52302b8a7df37
Simon Rozet <simon@rozet.name>
2008-02-05 23:41:34 +0100

    Same as last commit but on auth-related files and invokers

--- lib/ape/invokers/getter.rb
+++ lib/ape/invokers/getter.rb
@@ -1,83 +1,80 @@
 #   Copyright © 2006 Sun Microsystems, Inc. All rights reserved
 #   Use is subject to license terms - see file "LICENSE"
-
 require 'net/http'
 
 module Ape
-class Getter < Invoker
+  class Getter < Invoker
+    attr_reader :contentType, :charset, :body, :security_warning
 
-  attr_reader :contentType, :charset, :body, :security_warning
+    def get(contentType = nil, depth = 0, req = nil)
+      req = Net::HTTP::Get.new(AtomURI.on_the_wire(@uri)) unless req
+      @last_error = nil
 
-  def get(contentType = nil, depth = 0, req = nil)
-    req = Net::HTTP::Get.new(AtomURI.on_the_wire(@uri)) unless req
-    @last_error = nil
+      return false if document_failed?(depth, req)    
+      
+      begin
+        http = prepare_http
+       
+        http.start do |connection|
+          @response = connection.request(req)
 
-    return false if document_failed?(depth, req)    
-    
-    begin
-      http = prepare_http
-     
-      http.start do |connection|
-        @response = connection.request(req)
-
-        if need_authentication?(req)
-          @security_warning = true unless http.use_ssl?
-          return get(contentType, depth + 1, req) 
+          if need_authentication?(req)
+            @security_warning = true unless http.use_ssl?
+            return get(contentType, depth + 1, req) 
+          end
+          restart_authent_checker
+          
+          case @response
+          when Net::HTTPSuccess
+            return getBody(contentType)
+            
+          when Net::HTTPRedirection
+            redirect_to = @uri.merge(@response['location'])
+            @uri = AtomURI.check(redirect_to)
+            return get(contentType, depth + 1)
+            
+          else
+            @last_error = @response.message
+            return false
+          end      
         end
-        restart_authent_checker
-        
-        case @response
-        when Net::HTTPSuccess
-          return getBody(contentType)
-          
-        when Net::HTTPRedirection
-          redirect_to = @uri.merge(@response['location'])
-          @uri = AtomURI.check(redirect_to)
-          return get(contentType, depth + 1)
-          
-        else
-          @last_error = @response.message
-          return false
-        end      
+      rescue Exception
+        @last_error = "Can't connect to #{@uri.host} on port #{@uri.port}: #{$!}"
+        return false
       end
-    rescue Exception
-      @last_error = "Can't connect to #{@uri.host} on port #{@uri.port}: #{$!}"
+    end
+
+    def document_failed?(depth, req)    
+      if (depth > 10)      
+        if need_authentication?(req)
+  	#Authentication required
+          @last_error = "Authentication is required"
+        else
+  	# too many redirects
+          @last_error = "Too many redirects"
+        end
+        return true
+      end
       return false
     end
-  end
 
-  def document_failed?(depth, req)    
-    if (depth > 10)      
-      if need_authentication?(req)
-	#Authentication required
-        @last_error = "Authentication is required"
-      else
-	# too many redirects
-        @last_error = "Too many redirects"
+    def getBody contentType
+
+      if contentType
+        @contentType = @response['Content-Type']
+        # XXX TODO - better regex
+        if @contentType =~ /^([^;]*);/
+          @contentType = $1
+        end
+      
+        if contentType != @contentType
+          @last_error = "Content-type must be '#{contentType}', not '#{@contentType}'"
+        end
       end
+        
+      @body = @response.body
       return true
     end
-    return false
   end
-
-  def getBody contentType
-
-    if contentType
-      @contentType = @response['Content-Type']
-      # XXX TODO - better regex
-      if @contentType =~ /^([^;]*);/
-        @contentType = $1
-      end
-    
-      if contentType != @contentType
-        @last_error = "Content-type must be '#{contentType}', not '#{@contentType}'"
-      end
-    end
-      
-    @body = @response.body
-    return true
-  end
-  
-end
 end
 
