9df17187ef49d282f71413b51bff6f4aee731c61
Brandon Mitchell <brandon@systemisdown.net>
2008-02-23 15:42:02 -0800

    Add documentation for Ape::Crumbs.
    * Move original docs for RDoc to find.
    * Slight copy changes in original docs for clarity.
    * Documentation added for #grep, #<<, #each and #to_s
    * Skip documentation of #new, as it has little special behavior.
    
    NOTE: Is the behavior of @keep_next clear enough, sans documentation?

--- lib/ape/crumbs.rb
+++ lib/ape/crumbs.rb
@@ -1,23 +1,35 @@
 #   Copyright (c) 2006 Sun Microsystems, Inc. All rights reserved
 #   Use is subject to license terms - see file "LICENSE"
 
-# All the methods (getter/poster/putter/deleter) use the set_debug_output method
-#  of Net::HTTP to capture the dialogue.  This class is what gets passed to
-#  set_debug_output; it exists to define << to save only the interesting bits
-#  of the dialog.
-#
 module Ape
+  
+  # All descendants of Invoker use Net::HTTP::set_debug_output
+  # to capture the dialogue. This class is what gets passed to
+  # set_debug_output; it exists to define #<< to save only the interesting bits
+  # of the dialog.
   class Crumbs
-    def initialize
+    
+    def initialize #:nodoc:
       @crumbs = []
       @keep_next = false
     end
 
+    # call-seq:
+    #   crumbs.grep(pattern) => array
+    #
+    # Returns an array of crumbs for which <code>pattern === crumb</code>
+    #
+    # ==== Options
+    #  * pattern - A string or Regexp literal, as with Enumerable#grep.
     def grep(pattern)
       @crumbs.grep(pattern)
     end
 
-    def << data
+    # Appends +data+ to the report dialog
+    #
+    # ==== Options
+    #  * data - The message, as passed by an Invoker descendant. Required.
+    def <<(data)
       if @keep_next
         @crumbs << "> #{data}"
         @keep_next = false
@@ -28,10 +40,15 @@ module Ape
       end
     end
 
-    def each
+    # Yields each crumb sequentially to the supplied block.
+    def each #:yields: crumb
       @crumbs.each { |c| yield c }
     end
 
+    # call-seq:
+    #   crumbs.to_s => string
+    #
+    # Returns a string containing all crumbs, seperated by newlines.
     def to_s
       "  " + @crumbs.join("...\n")
     end
