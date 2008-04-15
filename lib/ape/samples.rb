7feecf91e41de10fbfb9534b3287ce2c217cd765
Simon Rozet <simon@rozet.name>
2008-04-15 20:45:20 +0200

    fix basic_entry sample

--- lib/ape/samples.rb
+++ lib/ape/samples.rb
@@ -6,21 +6,20 @@ module Ape
     module_function
 
     def basic_entry(locals={})
-      entry = Atom::Entry.new do |e|
-        e.id = locals[:id] if locals[:id]
-        e.title = locals[:title] || 'From the <APE> (サル)'
-        e.authors.new(:name => 'The Atom Protocol Exerciser')
-        e.updated = locals[:updated] || Time.now
-        e.summary = 'Summary from the APE'
-        e.content = %q{
-          <div xmlns='http://www.w3.org/1999/xhtml'>
-            <p>A test post from the &lt;APE&gt</p>
-            <p>If you see this in an entry, it's probably a left-over from an
-              unsuccessful Ape run; feel free to delete it.</p>
-          </div>
-        }
-      end
-      entry.content#['type'] = 'xhtml'#locals[:content_type] || 'xhtml'
+      entry = Atom::Entry.new
+      entry.id = locals[:id] if locals[:id]
+      entry.title = locals[:title] || 'From the <APE> (サル)'
+      entry.authors.new(:name => 'The Atom Protocol Exerciser')
+      entry.updated = locals[:updated] || Time.now
+      entry.summary = 'Summary from the APE'
+      entry.content = %q{
+        <div xmlns='http://www.w3.org/1999/xhtml'>
+          <p>A test post from the &lt;APE&gt</p>
+          <p>If you see this in an entry, it's probably a left-over from an
+            unsuccessful Ape run; feel free to delete it.</p>
+        </div>
+      }
+      entry.content['type'] = locals[:content_type] || 'xhtml'
       entry
     end
   end
