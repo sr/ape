ec0a16a9e882f2590ed52141e5fd5676034ce062
Simon Rozet <simon@rozet.name>
2008-04-16 22:02:19 +0200

    Move spec helpers functions to a module in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,45 @@ require 'ape/validators/entry_posting'
 class Rack::MockResponse
   alias :code :status
 end
+
+module EntryPostingValidatorHelper
+  def do_validate
+    @validator.run
+  end
+
+  def successful_response
+    [201, {'Location' => 'http://test.host/entries/1'}, [Ape::Samples.basic_entry.to_s]]
+  end
+
+  def response_for(what)
+    response = case what
+    when :successful
+      successful_response
+    when :unsuccessful
+      r = successful_response
+      r[0] = 500
+      r
+    when :no_location_header
+      # TODO: err, there must be a non-ugly way to do that
+      r = successful_response
+      r[1].reject! { |k, v| k == 'Location' }
+      r
+    else
+      raise ArgumentError
+    end 
+    app = lambda { |env| response }
+    env = Rack::MockRequest.env_for(uri='/entries', :method => 'POST')
+    Rack::MockResponse.new(*app.call(env))
+  end
+
+  def set_response!(response)
+    @response = response_for(response)
+    @http.stub!(:request).and_return(@response)
+  end
+
+  def with_response(response)
+    set_response!(response)
+    yield
+    do_validate
+  end
+end
--- spec/validators/entry_posting_spec.rb
+++ spec/validators/entry_posting_spec.rb
@@ -1,46 +1,8 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe 'When testing entry POSTing' do
-  def do_validate
-    @validator.run
-  end
+  include EntryPostingValidatorHelper
 
-  def successful_response
-    [201, {'Location' => 'http://test.host/entries/1'}, [Ape::Samples.basic_entry.to_s]]
-  end
-
-  def response_for(what)
-    response = case what
-    when :successful
-      successful_response
-    when :unsuccessful
-      r = successful_response
-      r[0] = 500
-      r
-    when :no_location_header
-      # TODO: err, there must be a non-ugly way to do that
-      r = successful_response
-      r[1].reject! { |k, v| k == 'Location' }
-      r
-    else
-      raise ArgumentError
-    end 
-    app = lambda { |env| response }
-    env = Rack::MockRequest.env_for(uri='/entries', :method => 'POST')
-    Rack::MockResponse.new(*app.call(env))
-  end
-
-  def set_response!(response)
-    @response = response_for(response)
-    @http.stub!(:request).and_return(@response)
-  end
-
-  def with_response(response)
-    set_response!(response)
-    yield
-    do_validate
-  end
-  
   before(:each) do
     @options = {:host => 'test.host', :port => '80', :collection => '/entries'}
     @reporter = mock('Ape::Reporter', :call => 1)
